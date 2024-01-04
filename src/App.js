d45d0ee8dbf9589da8bdbedd87344587d00abaa6
bablu <aniketpanchal07@gmail.com>
2024-01-04 11:38:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import './App.css';
 
 function App() {
   return (
+    <>
+
+<h1 className="text-3xl font-bold underline flex justify-center text-red-500">
+      Hello world!
+    </h1>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,7 +24,11 @@ function App() {
         </a>
       </header>
     </div>
+
+    </>
   );
+    
+    
 }
 
 export default App;
